0f3d5f73515bb991efe7f1e40e8705c874a34962
Farhan Hadi <farhan.abdhadi@gmail.com>
2021-02-22 10:52:26 +0800

    init

---
+++ app/Notifications/RealTimeNotification.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RealTimeNotification extends Notification implements ShouldBroadcast
+{
+    public $message;
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $message)
+    {
+        $this->user = $user;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database', 'broadcast'];
+    }
+
+    public function toBroadcast($notifiable)
+    {
+        return new BroadcastMessage([
+            'message' => $this->user . ' ' . $this->message,
+        ]);
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'message' => $this->user . ' ' . $this->message,
+        ];
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex items-center">
+        <div class="md:w-1/2 md:mx-auto">
+
+            @if (session('status'))
+                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+
+                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                    Dashboard
+                </div>
+
+                <div class="w-full p-6">
+                    <p class="text-gray-700">
+                        List of registered users:
+                    </p>
+                    <ul class="border border-gray-200 rounded-md">
+                        @foreach($users as $user)
+                            <li class="pl-3 pr-4 py-3 flex items-center justify-between text-sm leading-5">
+                                <div class="w-0 flex-1 flex items-center">
+                                    <!-- Heroicon name: paper-clip -->
+                                    <svg class="flex-shrink-0 h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+                                    <span class="ml-2 flex-1 w-0 truncate">
+                                    {{ $user->name }}
+                                    </span>
+                                </div>
+                                <div class="ml-4 flex-shrink-0">
+                                    <a href="/send-notification/{{ $user->id }}" class="font-medium text-indigo-600 hover:text-indigo-500 transition duration-150 ease-in-out">
+                                    Send notification
+                                    </a>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
